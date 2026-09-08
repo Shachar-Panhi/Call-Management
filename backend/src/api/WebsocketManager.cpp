b697ddeb6f9b5d604fb99b442ee2906bf7537f12
Shachar-Panhi <shachar.panhi@gmail.com>
2026-09-08 11:40:22 +0300

    Refactor - Added a log to notify when a session is added/removed from the manager

--- backend/src/api/WebsocketManager.cpp
+++ backend/src/api/WebsocketManager.cpp
@@ -1,12 +1,15 @@
 #include "WebsocketManager.hpp"
+#include "spdlog/spdlog.h"
 
 namespace CAM::API {
     WebsocketManager::WebsocketManager() = default;
 
     void WebsocketManager::join(std::shared_ptr<WebsocketSession> session) {
         sessions_.push_back(session);
+        spdlog::info("Session added to manager vector");
     }
     void WebsocketManager::leave(std::shared_ptr<WebsocketSession> session) {
         std::erase(sessions_, session);
+        spdlog::info("Session removed from manager vector");
     }
 }
