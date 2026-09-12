5b829c4ff5a6ab06476754a3be7c7bea354e2d9a
Shachar-Panhi <shachar.panhi@gmail.com>
2026-09-12 20:00:15 +0300

    Feat - Added glaze parsing to the peer connection and created a new struct for it to parse/write to.

--- backend/src/api/PeerConnection.cpp
+++ backend/src/api/PeerConnection.cpp
@@ -1,6 +1,8 @@
 #include "PeerConnection.hpp"
+#include "types.hpp"
 
 #include <spdlog/spdlog.h>
+#include <glaze/glaze.hpp>
 #include <utility>
 
 namespace CAM::API {
@@ -18,8 +20,19 @@ namespace CAM::API {
         rtc_connection_->onLocalDescription([weak_self = weak_from_this()](rtc::Description description) {
             if (auto self = weak_self.lock()) {
                 if (self->send_signaling_) {
-                    std::string sdp_message = std::string(description);
-                    self->send_signaling_(std::move(sdp_message));
+                    SignalingPacket packet;
+                    packet.type = description.typeString();
+                    packet.sdp = std::string(description);
+                    
+                    std::string json_message;
+                    auto glz_errc = glz::write_json(packet, json_message);
+
+                    if (glz_errc) {
+                        spdlog::error("Failed to write the signaling packet to json");
+                        return;
+                    }
+
+                    self->send_signaling_(std::move(json_message));
                 }
             }
         });
@@ -27,20 +40,46 @@ namespace CAM::API {
         rtc_connection_->onLocalCandidate([weak_self = weak_from_this()](rtc::Candidate candidate) {
             if (auto self = weak_self.lock()) {
                 if (self->send_signaling_) {
-                    std::string ice_message = std::string(candidate);
-                    self->send_signaling_(std::move(ice_message));
+                    SignalingPacket packet;
+                    packet.type = "candidate";
+                    packet.candidate = std::string(candidate);
+                    packet.sdpMid = candidate.mid();
+                    
+                    std::string json_message;
+                    auto glz_errc = glz::write_json(packet, json_message);
+                    
+                    if (glz_errc) {
+                        spdlog::error("Failed to write candidate to json");
+                        return;
+                    }
+                    
+                    self->send_signaling_(std::move(json_message));
                 }
             }
         });
     }
 
     void PeerConnection::handle_signaling_message(const std::string& message) {
-        spdlog::info("peer connection received: {}", message);
+        spdlog::info("peer connection received {}", message);
         
-        if (message == "start") {
-            spdlog::info("Generating WebRTC offer");
+       if (message == "start") {
             auto data_channel = rtc_connection_->createDataChannel("chat");
             rtc_connection_->setLocalDescription();
+            return;
+        }
+
+        SignalingPacket packet;
+        auto glz_errc = glz::read_json(packet, message);
+        
+        if (glz_errc) {
+            spdlog::error("json parsing error");
+            return;
+        }
+
+        if (packet.type == "answer" && packet.sdp.has_value()) {
+            rtc_connection_->setRemoteDescription(rtc::Description(packet.sdp.value(), packet.type));
+        } else if (packet.type == "candidate" && packet.candidate.has_value() && packet.sdpMid.has_value()) {
+            rtc_connection_->addRemoteCandidate(rtc::Candidate(packet.candidate.value(), packet.sdpMid.value()));
         }
     }
 }
